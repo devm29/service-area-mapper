214cb99af3889f6f68ce58744291d54b8bca072a
devm29 <devm29@users.noreply.github.com>
2022-03-02 16:00:00 -0500

    Feature::Test::Added unit tests for ServiceArea model and API
    Added ServiceArea API tests covering successful creation with provider relationship and required-provider validation behavior.

--- testapp/tests.py
+++ testapp/tests.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Provider
+from .models import Provider, ServiceArea
 
 
 class ProviderAPITests(APITestCase):
@@ -56,3 +56,40 @@ class ProviderAPITests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("phone", response.data)
+
+
+class ServiceAreaAPITests(APITestCase):
+    service_area_url = "/testapp/service_area/"
+
+    def setUp(self):
+        self.provider = Provider.objects.create(
+            name="Provider One",
+            email="provider1@example.com",
+            phone="+12345678901",
+            language="English",
+            currency="USD",
+        )
+
+    def test_create_service_area_success(self):
+        payload = {
+            "name": "Downtown",
+            "price": "19.99",
+            "provider": self.provider.id,
+        }
+
+        response = self.client.post(self.service_area_url, payload, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ServiceArea.objects.count(), 1)
+        self.assertEqual(ServiceArea.objects.first().provider_id, self.provider.id)
+
+    def test_create_service_area_requires_provider(self):
+        payload = {
+            "name": "Downtown",
+            "price": "19.99",
+        }
+
+        response = self.client.post(self.service_area_url, payload, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("provider", response.data)
