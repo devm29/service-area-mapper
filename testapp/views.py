da1f3066e16f116c81b23696d5af196c72cd16a0
devm29 <devm29@users.noreply.github.com>
2022-03-02 13:00:00 -0500

    Feature::Enhancement::Added pagination to service area search
    Updated  to paginate GeoDjango query results and enabled DRF PageNumberPagination defaults via  settings.

--- testapp/views.py
+++ testapp/views.py
@@ -37,5 +37,10 @@ class ServiceAreaViews(viewsets.ModelViewSet):
             .filter(area_polygon__contains=pnt)
             .all()
         )
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
