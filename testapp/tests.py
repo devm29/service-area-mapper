48715dc912f129c9657ab6e54e420d2b7921dd03
devm29 <devm29@users.noreply.github.com>
2022-03-02 17:00:00 -0500

    Feature::Test::Added unit tests for geographic search functionality
    Added API tests for point-in-polygon search behavior, including inside/outside polygon results and invalid coordinate validation handling.

--- testapp/tests.py
+++ testapp/tests.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.contrib.gis.geos import Polygon
 
 from .models import Provider, ServiceArea
 
@@ -93,3 +94,50 @@ class ServiceAreaAPITests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("provider", response.data)
+
+
+class SearchServiceAreaAPITests(APITestCase):
+    search_url = "/testapp/search_service_area/"
+
+    def setUp(self):
+        self.provider = Provider.objects.create(
+            name="Provider One",
+            email="provider1@example.com",
+            phone="+12345678901",
+            language="English",
+            currency="USD",
+        )
+        ServiceArea.objects.create(
+            name="City Center",
+            price="10.00",
+            provider=self.provider,
+            area_polygon=Polygon(
+                (
+                    (0.0, 0.0),
+                    (0.0, 10.0),
+                    (10.0, 10.0),
+                    (10.0, 0.0),
+                    (0.0, 0.0),
+                )
+            ),
+        )
+
+    def test_search_service_area_returns_match_inside_polygon(self):
+        response = self.client.get(self.search_url, {"lat": "5.0", "lng": "5.0"})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("results", response.data)
+        self.assertEqual(len(response.data["results"]), 1)
+
+    def test_search_service_area_returns_empty_outside_polygon(self):
+        response = self.client.get(self.search_url, {"lat": "50.0", "lng": "50.0"})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("results", response.data)
+        self.assertEqual(len(response.data["results"]), 0)
+
+    def test_search_service_area_rejects_invalid_coordinates(self):
+        response = self.client.get(self.search_url, {"lat": "abc", "lng": "5.0"})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("message", response.data)
